1e74597cda82813282626006c9efc0165980e25f
DHay10 <son-ta@hotmail.fr>
2022-10-11 12:28:43 -0400

    Profile Controller: update()

--- app/controllers/Profile.php
+++ app/controllers/Profile.php
@@ -3,6 +3,12 @@
 
     class Profile extends \app\core\Controller {
 
+        public function index() {
+            $profile = new \app\models\Profile();
+            $publications = $profile->getPublications();
+            $this->view('Profile/index', $publications);
+        }
+
         #[\app\filters\User]
 		public function createProfile() {
             $this->view('Profile/profileCreation');
@@ -10,4 +16,21 @@
 				$profile = new \app\models\Profile();
 			}
 		}
+
+        public function update() {
+            $profile = new \app\models\Profile();
+            $profile = $profile->get($profile_id);
+            if(isset($_POST['action'])) {
+				$profile->first_name = $_POST['first_name'];
+                $profile->middle_name = $_POST['middle_name'];
+			    $profile->last_name = $_POST['last_name'];
+                $profile->update();
+
+                header('location:/Profile/index');
+			} else {
+                $this->view('Profile/profileEditing');
+            }
+            $profile = new \app\models\Profile();
+            
+        }
     }
