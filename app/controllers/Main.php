374bb65d0761b8bbffac082b79f215a6fb96d162
DHay10 <son-ta@hotmail.fr>
2022-10-11 14:08:51 -0400

    Reorganized the Views

---
+++ app/controllers/Main.php
@@ -0,0 +1,18 @@
+<?php
+    namespace app\controllers;
+
+    class Main extends \app\core\Controller { 
+        public function index() {
+            $profile = new \app\models\Profile();
+            $publications = $profile->getPublications();
+            $this->view('Main/home', $publications);
+        }
+
+        public function searchByID() {
+
+        }
+
+        public function searchByCaption() {
+            
+        }
+    }
---
+++ app/views/includes/header.php
@@ -0,0 +1,6 @@
+<div class="nav">
+    <a href='/Main/index'>Home</a>
+    <a href='/Profile/index'>Profile</a>
+    <a href='/Contact/index'>Contact Us</a>
+    <a href='/User/index'>Log In</a>
+</div>
