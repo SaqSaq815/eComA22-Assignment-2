5db2ed234109cb41ebaeced15948f9819a4f3bcc
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-10-11 20:18:46 -0400

    profile update

---
+++ app/views/Profile/update.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Profile Edit</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<?php require 'app\views\includes\header.php'; ?>
+	<?php require 'app\views\includes\error.php'; ?>
+
+	<h1>Your Profile</h1>
+
+	<form action='' method='post'>
+			<label>First Name:<input type="text" name="first_name" required/></label><br>
+			<label>Middle Name:<input type="text" name="middle_name" required /></label><br>
+			<label>Last Name:<input type="text" name="last_name" required/></label><br>
+			<input type="submit" name="action" value="Edit Profile" />
+	</form>
+
+</body>
+</html>
