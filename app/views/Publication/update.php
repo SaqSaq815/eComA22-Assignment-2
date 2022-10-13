2b550a1ed0993563c370f12e18367a67cbb99a02
DHay10 <son-ta@hotmail.fr>
2022-10-12 21:36:44 -0400

    styled publication update, detail

--- app/views/Publication/update.php
+++ app/views/Publication/update.php
@@ -3,6 +3,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Update Post</title>
+        <link rel="stylesheet" href="/public/css/style.css">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 	</head>
@@ -10,16 +11,15 @@
 	<body>
 		<?php require 'app\views\includes\header.php'; ?>
 		<?php require 'app\views\includes\error.php'; ?>
+        <div class="main">
+            <h1>Edit Publication</h1>
 
-		<h1>Edit Profile</h1>
-
-		<form action='' method='post'>	
-			<?php echo "
-            
-				<label>Caption:<input type=\"textarea\" name=\"caption\"/></label><br>
-				<input type=\"submit\" name=\"action\" value=\"Edit Post\" />";
-			?>
-		</form>
+            <form action='' method='post'>	
+                <label for="caption">Caption</label>
+                <?php echo "<input type=\"textarea\" id=\"caption\" name=\"caption\" value=\"$data->caption\" /></label><br>"; ?>
+                <input type="submit" name="action" value="Edit Post"/>
+            </form>
+        </div>
 
 	</body>
 </html>
