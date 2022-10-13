36247bab11477659cbb9e0c53c2817bcac89b708
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-10-12 23:30:07 -0400

    update intasham file

--- instasham.sql
+++ instasham.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2022 at 10:46 PM
+-- Generation Time: Oct 13, 2022 at 05:27 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -21,9 +21,6 @@ SET time_zone = "+00:00";
 -- Database: `instasham`
 --
 
-CREATE DATABASE IF NOT EXISTS `instasham` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `instasham`;
-
 -- --------------------------------------------------------
 
 --
@@ -37,6 +34,16 @@ CREATE TABLE `comment` (
   `comment_text` text NOT NULL,
   `date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `publication_id`, `profile_id`, `comment_text`, `date_time`) VALUES
+(17, 15, 1, 'nice hair', '2022-10-13 02:51:35'),
+(18, 15, 1, 'jeff', '2022-10-13 03:04:44'),
+(19, 15, 1, 'hiii', '2022-10-13 03:04:47'),
+(24, 17, 3, 'hey kids in basement', '2022-10-13 05:21:27');
 
 -- --------------------------------------------------------
 
@@ -52,6 +59,15 @@ CREATE TABLE `profile` (
   `last_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`profile_id`, `user_id`, `first_name`, `middle_name`, `last_name`) VALUES
+(1, 1, 'Saqliyan', 'Md', 'Islam'),
+(2, 1, 'Md Saqliyan', '', 'Islam'),
+(3, 2, 'saq2', '', 'sa');
+
 -- --------------------------------------------------------
 
 --
@@ -66,6 +82,16 @@ CREATE TABLE `publication` (
   `date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `publication`
+--
+
+INSERT INTO `publication` (`publication_id`, `profile_id`, `picture`, `caption`, `date_time`) VALUES
+(14, 1, '6347441bae867.jpg', 'indeed', '2022-10-13 00:47:55'),
+(15, 1, '63475ac32210f.png', 'jeff', '2022-10-13 02:24:35'),
+(16, 1, '63477eac12ba3.png', 'hello i have drip', '2022-10-13 04:57:48'),
+(17, 3, '634781e0a5c6c.png', 'the drip is real', '2022-10-13 05:11:28');
+
 -- --------------------------------------------------------
 
 --
@@ -79,6 +105,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password_hash`) VALUES
+(1, 'saq', '$2y$10$7/pUGdRqb4XYcCXSiTp.OeX2gAtpB8QErAxAm.9TfRlw776LERyBC'),
+(2, 'saq2', '$2y$10$gfQivjQZLXyeeQ//J.A6vuN7e6jccG90rvtKdMnez0gq9CYandFsC');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -87,8 +121,8 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`comment_id`),
-  ADD KEY `profile_id` (`profile_id`),
-  ADD KEY `publication_id` (`publication_id`);
+  ADD KEY `comment_ibfk_2` (`publication_id`),
+  ADD KEY `comment_ibfk_1` (`profile_id`);
 
 --
 -- Indexes for table `profile`
@@ -102,7 +136,7 @@ ALTER TABLE `profile`
 --
 ALTER TABLE `publication`
   ADD PRIMARY KEY (`publication_id`),
-  ADD KEY `profile_id` (`profile_id`);
+  ADD KEY `publication_ibfk_1` (`profile_id`);
 
 --
 -- Indexes for table `user`
@@ -118,25 +152,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `publication`
 --
 ALTER TABLE `publication`
-  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -146,8 +180,8 @@ ALTER TABLE `user`
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`),
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`publication_id`) REFERENCES `publication` (`publication_id`);
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`publication_id`) REFERENCES `publication` (`publication_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `profile`
@@ -159,7 +193,7 @@ ALTER TABLE `profile`
 -- Constraints for table `publication`
 --
 ALTER TABLE `publication`
-  ADD CONSTRAINT `publication_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
+  ADD CONSTRAINT `publication_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
